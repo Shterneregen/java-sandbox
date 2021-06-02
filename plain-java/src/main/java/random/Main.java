229b11e8eb227124985fd96cd9397863f6cff1ec
shterneregen <shterneregen@gmail.com>
2021-06-03 00:50:29 +0300

    adding main gradle project with subprojects

--- plain-java/src/main/java/random/Main.java
+++ plain-java/src/main/java/random/Main.java
@@ -1,8 +1,14 @@
 package random;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
+	    LocalDate now = LocalDate.now();
+	    DayOfWeek dayOfWeek = now.plusYears(1).getDayOfWeek();
+	    System.out.println(dayOfWeek);
     }
 
 }
